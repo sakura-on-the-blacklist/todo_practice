3990bbea11ad01488bd5dd03145122aaf3cea24b
sakusakuhongoroo <80045643+sakusakuhongoroo@users.noreply.github.com>
2021-06-15 20:45:25 +0800

    Update Models/Task.php
    Co-authored-by: chihiro0415 <30280895+chihiro0415@users.noreply.github.com>

--- Models/Task.php
+++ Models/Task.php
@@ -22,7 +22,7 @@ class Task extends Model
         $stmt->execute($data);
     }
     // * update()を以下に追加する
-    public function update($data);
+    public function update($data)
 
     {
         $stmt = $this->db_manager->dbh->prepare(' UPDATE ' . $this->table . 'SET title = ?. contents = ? WHERE id = ?');
